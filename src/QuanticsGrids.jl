2bf90cfb34cbe8979e71759e34cff24ede1f9ca8
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2024-04-01 21:02:20 +0900

    Export public API

--- src/QuanticsGrids.jl
+++ src/QuanticsGrids.jl
@@ -2,6 +2,11 @@ module QuanticsGrids
 
 using StaticArrays
 
+export DiscretizedGrid, InherentDiscreteGrid
+export quantics_to_grididx, quantics_to_origcoord
+export grididx_to_quantics, grididx_to_origcoord
+export origcoord_to_quantics, origcoord_to_grididx
+
 include("quantics.jl")
 include("grid.jl")
 
