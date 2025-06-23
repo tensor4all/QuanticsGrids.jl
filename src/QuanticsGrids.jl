7052351e93f6f44e9f7deea4604ffb56b25fc98e
Samuel Badr <samuel.badr@gmail.com>
2025-06-23 15:31:41 +0200

    increase version to v0.5.0 and drop StaticArrays dependency

--- src/QuanticsGrids.jl
+++ src/QuanticsGrids.jl
@@ -1,7 +1,5 @@
 module QuanticsGrids
 
-using StaticArrays
-
 export DiscretizedGrid, InherentDiscreteGrid
 export quantics_to_grididx, quantics_to_origcoord
 export grididx_to_quantics, grididx_to_origcoord
